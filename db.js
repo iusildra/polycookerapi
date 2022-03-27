cdca65c2f89aed9d0f20866e172871f0878e9206
Lucas Nouguier <lucas.nouguier@protonmail.com>
2022-03-27 17:03:55 +0200

    Added dotenv

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "polycooker",
     password: "![*w>&k=\\JK1Gdu~-F>",
-    host: "/run/postgresql",
+    host: process.env.DATABASE_URL,
     database: "PolyCooker",
     port: "5432",
 });
