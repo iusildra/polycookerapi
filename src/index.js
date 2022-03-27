4b8e64531e3fe1bcb880ecd237f14d7ca26d7291
Lucas Nouguier <lucas.nouguier@protonmail.com>
2022-03-27 18:41:46 +0200

    Changed architecture to match webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const router = require("./routes/router");
-const config = require("./config/default.json")
+const config = require("./config/default.json");
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const port = 3080;
+const port =
+    process.env.PORT == null || process.env.PORT == ""
+        ? 8000
+        : process.env.PORT;
 
 bodyParser.urlencoded({
-    extended: false
+    extended: false,
 });
 
-app.use(cors({ origin:config.client, optionsSuccessStatus: 204}));
+app.use(cors({ origin: config.client, optionsSuccessStatus: 204 }));
 app.use(bodyParser.json());
 app.use("/api", router);
 
